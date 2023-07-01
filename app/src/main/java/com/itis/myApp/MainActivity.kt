601569a2622cfd24cd7481df6009b52d99da2994
Daniil Fomin <Daniil_fomin_04@mail.ru>
2023-07-01 10:27:09 +0300

    Add a first exercise 29.06

--- app/src/main/java/com/itis/myApp/MainActivity.kt
+++ app/src/main/java/com/itis/myApp/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.itis.myApp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.itis.myApp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //  private var binding: ActivityMainBinding? = null
+
+    private lateinit var etUsername : EditText
+    private lateinit var etHeight : EditText
+    private lateinit var etWeight : EditText
+    private lateinit var etAge : EditText
+    private lateinit var btnValidate : MaterialButton
+    private var resultInfo : TextView? = null
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etUsername = findViewById(R.id.etUsername)
+        etHeight = findViewById(R.id.etHeight)
+        etWeight = findViewById(R.id.etWeight)
+        etAge = findViewById(R.id.etAge)
+        btnValidate = findViewById(R.id.btnValidate)
+        resultInfo = findViewById(R.id.resultInfo)
+
+        btnValidate.setOnClickListener {
+
+            val username = etUsername.text.toString().trim()
+            val height = etHeight.text.toString().trim()
+            val weight = etWeight.text.toString().trim()
+            val age = etAge.text.toString().trim()
+
+            if (username.isEmpty() || username.length > 50) {
+                etUsername.error = "Invalid username"
+                return@setOnClickListener
+            } else if (height.isEmpty() || height.toInt() > 250) {
+                etHeight.error = "Invalid height"
+                return@setOnClickListener
+            } else if (weight.isEmpty() || weight.toDouble() > 250.0) {
+                etWeight.error = "Invalid weight"
+                return@setOnClickListener
+            } else if (age.isEmpty() || age.toInt() > 150) {
+                etAge.error = "Invalid age"
+                return@setOnClickListener
+            } else {
+                val callories : Int = ((88.36 + (13.4 + weight.toDouble()) + (4.8 * height.toDouble()) - (5.7 * age.toDouble()))*3).toInt()
+                val nds : Int = username.length * 50 / 100
+                val monthlyPayment : Int = ((height.toDouble() + weight.toDouble()) * 10).toInt()
+                val horoscope : String = if (age.toInt() < 20) "You'll have a good day!" else "Your day won't be shiny and bright..."
+                val personalTax : Int = ((height.toDouble() * 100 + username.length * 10) * 5).toInt()
+                resultInfo?.text = "Number of calories you need daily: $callories\n" +
+                        "Your NDS: $nds%\n" +
+                        "Your monthly payment: $monthlyPayment\n" +
+                        "Horoscope says today: $horoscope\n" +
+                        "Your main personal tax this year: $personalTax"
+                Toast.makeText(this, "Validation Completed", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
