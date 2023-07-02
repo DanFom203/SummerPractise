fb7d7840b5a23c481e215df1f3dd3800eeb3aef9
Daniil Fomin <Daniil_fomin_04@mail.ru>
2023-07-02 21:55:42 +0300

    Third exercise completed

--- app/src/main/java/com/itis/myApp/FruitRepository.kt
+++ app/src/main/java/com/itis/myApp/FruitRepository.kt
@@ -7,15 +7,7 @@ object FruitRepository {
             "The most common citrus crop in all tropical and subtropical regions of the world."),
         Fruit(2,"Apple","green&yellow&red","multi-seeded_non-opening",
         "https://w.forfun.com/fetch/31/31787d8fe2cc30785317823f59cdea3b.jpeg",
-        "The juicy fruit of the apple tree, which is eaten fresh and baked, serves as a raw material in cooking and for making drinks"),
-        Fruit(2,"Apple","green&yellow&red","multi-seeded_non-opening",
-        "https://w.forfun.com/fetch/31/31787d8fe2cc30785317823f59cdea3b.jpeg",
-        "The juicy fruit of the apple tree, which is eaten fresh and baked, serves as a raw material in cooking and for making drinks"),
-        Fruit(2,"Apple","green&yellow&red","multi-seeded_non-opening",
-            "https://w.forfun.com/fetch/31/31787d8fe2cc30785317823f59cdea3b.jpeg",
-            "The juicy fruit of the apple tree, which is eaten fresh and baked, serves as a raw material in cooking and for making drinks"),
-        Fruit(1, "Orange","orange","Citrus",
-            "https://webpulse.imgsmail.ru/imgpreview?key=pulse_cabinet-image-bae5828e-a948-4acb-9496-7d74087d3493&mb=webpulse",
-            "The most common citrus crop in all tropical and subtropical regions of the world.")
+        "The juicy fruit of the apple tree, which is eaten fresh and baked, serves as a raw material in cooking and for making drinks")
+
     )
 }
--- app/src/main/java/com/itis/myApp/ProfileFragment.kt
+++ app/src/main/java/com/itis/myApp/ProfileFragment.kt
@@ -1,13 +1,11 @@
 package com.itis.myApp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.itis.myApp.databinding.FragmentProfileBinding
-import com.itis.myApp.utils.showSnackbar
 import kotlin.random.Random
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
@@ -18,17 +16,12 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentProfileBinding.bind(view)
-
-        val name = arguments?.getString(ARG_NAME)
-        Log.e("ProfileFragment", name.orEmpty())
-
         initAdapter()
-
-        binding?.tvTitle?.setOnClickListener {
-            adapter?.updateDataset(
-                FruitRepository.list.subList(0, Random.nextInt(5))
-            )
-        }
+        //  binding?.tvTitle?.setOnClickListener {
+        //      adapter?.updateDataset(
+        //          FruitRepository.list.subList(0, Random.nextInt(5))
+        //      )
+        //  }
     }
 
     override fun onDestroyView() {
@@ -40,23 +33,13 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         adapter = FruitAdapter(
             list = FruitRepository.list,
             glide = Glide.with(this),
-            onItemClick = { city ->
-                binding?.root?.showSnackbar(city.name)
+            onItemClick = { fruit ->
+                findNavController().navigate(
+                    R.id.action_profileFragment_to_informationFragment,
+                    InformationFragment.createBundle(fruit.id)
+                )
             }
         )
         binding?.rvFruit?.adapter = adapter
-        binding?.rvFruit?.layoutManager = GridLayoutManager(requireContext(), 2)
-    }
-
-    companion object {
-        private const val ARG_NAME = "ARG_NAME"
-        private const val ARG_AGE = "ARG_AGE"
-
-        fun createBundle(name : String, age : Int) : Bundle {
-            val bundle = Bundle()
-            bundle.putString(ARG_NAME, name)
-            bundle.putInt(ARG_AGE, age)
-            return bundle
-        }
     }
 }
